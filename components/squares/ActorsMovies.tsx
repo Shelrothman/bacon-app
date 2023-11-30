9d104ca8e3013d4f31410229560ee01df768af1f
Shelby Rothman <shelby.rothman@aderant.com>
2023-11-30 15:20:59 -0800

    rendering new cast to card from movies

--- components/squares/ActorsMovies.tsx
+++ components/squares/ActorsMovies.tsx
@@ -1,5 +1,6 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Pressable } from 'react-native'
 import { BaconFeatureList } from '../../types'
+import { useAppContext } from '../../contexts/AppContext'
 
 type ActorsMoviesProps = BaconFeatureList & {
     actorName: string;
@@ -7,17 +8,43 @@ type ActorsMoviesProps = BaconFeatureList & {
 
 export function ActorsMovies(props: ActorsMoviesProps) {
 
-    // console.log('in actorsMovies comp', props)
+    const {
+        getCast,
+        setCurrentCardCast,
+        setSquareState,
+        setIsLoading,
+        setCurrentMovieTitle
+    } = useAppContext();
+
+    const handleFeatureActorNodePress = (id: number, movieTitle: string) => {
+        setIsLoading && setIsLoading(true);
+        getCast && getCast(movieTitle).then((result) => {
+            if (result) {
+                setCurrentCardCast && setCurrentCardCast(result);
+                setSquareState && setSquareState('movieCast');
+                setCurrentMovieTitle && setCurrentMovieTitle(movieTitle);
+            }
+        }).finally(() => {
+            setIsLoading && setIsLoading(false);
+        });
+    }
 
     return (
         <View style={{ backgroundColor: 'orange' }}>
             <Text>Movies for {props.actorName}</Text>
             <ScrollView>
+                {/* TODO: a node component for each movie node */}
                 {props.features.map((feature) => {
                     return (
-                        <Text key={feature.id}>{feature.title} {`\n`} {`\t`}
-                            character: {feature.characterName || 'N/A'}
-                        </Text>
+                        <Pressable
+                            onPress={() => handleFeatureActorNodePress(feature.id, feature.title)}
+                            key={feature.id} id={feature.title} style={{ borderColor: 'blue', borderWidth: 1 }}
+                        >
+
+                            <Text key={feature.id}>{feature.title} {`\n`} {`\t`}
+                                character: {feature.characterName || 'N/A'}
+                            </Text>
+                        </Pressable>
                     )
                 })}
             </ScrollView>
